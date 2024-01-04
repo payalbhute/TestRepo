169ee047953a7564f93ace27d269b361cfb66ad6
HP <HP@192.168.0.102>
2024-01-04 17:38:34 +0530

    Added 3 times

--- TestArtifactID1/src/main/java/mavenPackage/MavenClass1.java
+++ TestArtifactID1/src/main/java/mavenPackage/MavenClass1.java
@@ -5,6 +5,8 @@ public class MavenClass1 {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello Maven");
+		System.out.println("Hello Maven");
+		System.out.println("Hello Maven");
 		
 	}
 }
